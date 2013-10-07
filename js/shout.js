ccf9fab90508db394956c4bae16d9d7fddf32789
Thileepan <sktgthill@gmail.com>
2013-10-07 08:44:14 +0530

    Shout listing UI alignment issue fixed

--- js/shout.js
+++ js/shout.js
@@ -92,11 +92,11 @@ function getShouts()
 		for (var i = 0; i < results.length; i++) {
 		  var object = results[i];
 		  //console.log(object);
-		console.log('CreatedOn:' + object.get('createdOn'));
+		//console.log('CreatedOn:' + object.get('createdOn'));
 		 //var _time = object.get('createdOn');
 		 //console.log(_time);
 		 shoutBoxHTML += '<li class="list-group-item">';
-			shoutBoxHTML += '<div class="row"><span class="col-xs-1"><img src="https://ssl.gstatic.com/ui/v1/icons/mail/profile_mask2.png" alt="..." class="img-circle"></span><span class="col-xs-9">' + '<b>' + object.get('createdBy') + '</b><p class="text-muted"><small>' + object.get('message') + '</small></p></span><span class="col-xs-2"><small><abbr class="pull-right" data-livestamp="'+object.get('createdOn')+'"></abbr></small></span></div>';
+			shoutBoxHTML += '<div class="row"><div class="col-xs-1"><img src="https://ssl.gstatic.com/ui/v1/icons/mail/profile_mask2.png" alt="..." class="img-circle"></div><div class="col-xs-9">' + '<b>' + object.get('createdBy') + '</b><p class="text-muted"><small>' + object.get('message') + '</small></p></div><div class="col-xs-2"><small><abbr class="pull-right" data-livestamp="'+object.get('createdOn')+'"></abbr></small></div></div>';
 		 shoutBoxHTML += '</li>';
 		}
 		shoutBoxHTML += '</ul>';
